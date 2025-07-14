8244acadc7640a38ff90c48de494aa1286289950
Dvl <84147332+Sylbird@users.noreply.github.com>
2025-07-14 16:51:04 -0500

    Implemented orders list view with data table:
    Add DataTable component to display orders with CRUD actions.
    Include toolbar with create new order button
    Fetch orders from backend API on component mount.

--- src/components/orders/index.tsx
+++ src/components/orders/index.tsx
@@ -1,7 +1,93 @@
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { useEffect, useState } from 'react';
+import { Button } from 'primereact/button';
+import { useNavigate } from 'react-router';
+import { Toolbar } from 'primereact/toolbar';
+
 const Orders = () => {
+  const navigate = useNavigate();
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_BACKEND_API}orders`
+        );
+        if (!response.ok) throw new Error('Failed to fetch');
+        const data = await response.json();
+        setOrders(data);
+      } catch (error) {
+        console.error('Error fetching orders:', error);
+      }
+    };
+    fetchOrders();
+  }, []);
+
+  const actionButtons = (rowData: any) => {
+    return (
+      <>
+        <Button
+          icon="pi pi-pencil"
+          rounded
+          outlined
+          className="mr-2"
+          onClick={() => navigate(`/add-order/:${rowData.id}`)}
+        />
+        <Button
+          icon="pi pi-trash"
+          rounded
+          outlined
+          severity="danger"
+          onClick={() => {}}
+        />
+      </>
+    );
+  };
+
+  const leftToolbarTemplate = () => {
+    return (
+      <div className="flex flex-wrap gap-2">
+        <h2>You can also create a new Order:</h2>
+      </div>
+    );
+  };
+
+  const rightToolbarTemplate = () => {
+    return (
+      <Button
+        label="New"
+        icon="pi pi-plus"
+        severity="success"
+        onClick={() => {}}
+      />
+    );
+  };
+
   return (
     <div>
       <h1>My Orders</h1>
+      <div>
+        <Toolbar
+          className="mb-4"
+          start={leftToolbarTemplate}
+          end={rightToolbarTemplate}
+        ></Toolbar>
+        <DataTable value={orders} tableStyle={{ minWidth: '50rem' }}>
+          <Column field="id" header="Id"></Column>
+          <Column field="order_number" header="#Order"></Column>
+          <Column field="date" header="Date"></Column>
+          <Column field="num_products" header="#Products"></Column>
+          <Column field="final_price" header="FinalPrice"></Column>
+          <Column field="status" header="Status"></Column>
+          <Column
+            body={actionButtons}
+            exportable={false}
+            style={{ minWidth: '12rem' }}
+          ></Column>
+        </DataTable>
+      </div>
     </div>
   );
 };
