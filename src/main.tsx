0469224f71bc60cc63a2f78af43bd316ce9a8882
Dvl <84147332+Sylbird@users.noreply.github.com>
2025-07-14 13:42:19 -0500

    Added GlobalStyle

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import router from './router';
+import GlobalStyles from './styles/GlobalStyle';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalStyles />
     <RouterProvider router={router} />
   </StrictMode>
 );
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+  height: 100vh;
+  width: 100vw;
+  }
+`;
+
+export default GlobalStyle;
