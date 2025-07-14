e66cbf10476a2d12ae6241f947a18e24f9afccb2
Dvl <84147332+Sylbird@users.noreply.github.com>
2025-07-14 13:51:30 -0500

    Add primereact provider & theme css

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
+import { PrimeReactProvider } from 'primereact/api';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import router from './router';
 import GlobalStyles from './styles/GlobalStyle';
+import 'primereact/resources/themes/lara-light-cyan/theme.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GlobalStyles />
-    <RouterProvider router={router} />
+    <PrimeReactProvider>
+      <RouterProvider router={router} />
+    </PrimeReactProvider>
   </StrictMode>
 );
