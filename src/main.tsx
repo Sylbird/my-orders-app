802547f9b05019987ac31602901b5407a5c8e95a
Dvl <84147332+Sylbird@users.noreply.github.com>
2025-07-14 16:18:47 -0500

    Add primeflex and primeicons dependencies & Move PrimeReact theme import to GlobalStyle

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import router from './router';
 import GlobalStyles from './styles/GlobalStyle';
-import 'primereact/resources/themes/lara-light-cyan/theme.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,5 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/lara-light-cyan/theme.css';
+
 const GlobalStyle = createGlobalStyle`
   body {
   height: 100vh;
