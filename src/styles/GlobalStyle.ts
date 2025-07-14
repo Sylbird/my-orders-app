83515173c286357370ba2f6110668188a961f91b
Dvl <84147332+Sylbird@users.noreply.github.com>
2025-07-14 16:41:36 -0500

    Removed Unnecessary Style

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -5,10 +5,6 @@ import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/lara-light-cyan/theme.css';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-  height: 100vh;
-  width: 100vw;
-  }
 `;
 
 export default GlobalStyle;
